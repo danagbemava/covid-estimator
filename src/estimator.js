141d1ff64933a4176d7e70c9956ce84f510c9724
danagbemava <danagbemava@gmail.com>
2020-04-07 15:23:51 +0000

    completed challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,43 @@
-const covid19ImpactEstimator = (data) => data;
+const convertMonthsToDays = (months) => months * 30;
+const convertWeeksToDays = (weeks) => weeks * 7;
+
+const getNumberOfDays = (periodType, timeToElapse) => {
+  switch (periodType) {
+    case 'months':
+      return convertMonthsToDays(timeToElapse);
+    case 'weeks':
+      return convertWeeksToDays(timeToElapse);
+    default:
+      return timeToElapse;
+  }
+};
+
+const covid19ImpactEstimator = (data) => {
+  const {
+    reportedCases,
+    periodType,
+    timeToElapse
+  } = data;
+
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+
+  const infectionRate = Math.trunc(getNumberOfDays(periodType, timeToElapse) / 3);
+
+
+  severeImpact.infectionsByRequestedTime = (2 ** infectionRate) * (reportedCases * 10);
+  impact.infectionsByRequestedTime = (2 ** infectionRate) * (reportedCases * 50);
+
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
+
 
 export default covid19ImpactEstimator;
