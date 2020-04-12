0a7dc218ccb0ca4c6dd663021ff07a1fca6669fa
danagbemava <danagbemava@gmail.com>
2020-04-12 19:48:32 +0000

    added comments

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,26 @@
+/**
+ *
+ * @param months
+ *
+ * Takes the number of months and convert it to days
+ */
 const convertMonthsToDays = (months) => months * 30;
+
+/**
+ *
+ * @param weeks
+ *
+ * Takes the number of weeks anc converts it to days
+ */
 const convertWeeksToDays = (weeks) => weeks * 7;
 
+/**
+ *
+ * @param periodType
+ * @param timeToElapse
+ *
+ * Takes the periodType and the timeToElapse and returns the days equivalent.
+ */
 const getNumberOfDays = (periodType, timeToElapse) => {
   switch (periodType) {
     case 'months':
@@ -12,6 +32,14 @@ const getNumberOfDays = (periodType, timeToElapse) => {
   }
 };
 
+/**
+ *
+ * @param hospitalBeds
+ * @param casesByTime
+ *
+ * Takes the number of hospitalBeds and cases by times and
+ * return the number of hosipita beds that will be availaible by requested time.
+ */
 const calcHospitalSpace = (hospitalBeds, casesByTime) => Math.trunc(
   (hospitalBeds * 0.35) - casesByTime
 );
@@ -63,6 +91,13 @@ const calculateImpact = (data) => {
 
   return impact;
 };
+
+/**
+ *
+ * @param data
+ *
+ * Calculates the wort case scenario for the given data input
+ */
 
 const calculateSevereImpact = (data) => {
   const {
